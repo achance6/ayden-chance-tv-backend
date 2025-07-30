8b9d526f0f2df690e310f29ea7536f2f0275430e
achance6 <ayden.chance@rocketmail.com>
2025-07-29 20:57:35 -0400

    Updated ActvApp.java

--- stack/src/main/java/com/chance/ayden/cdk/ActvApp.java
+++ stack/src/main/java/com/chance/ayden/cdk/ActvApp.java
@@ -4,35 +4,26 @@ import software.amazon.awscdk.App;
 import software.amazon.awscdk.Environment;
 import software.amazon.awscdk.StackProps;
 
+import java.util.Optional;
+
 public class ActvApp {
+  @SuppressWarnings("unused")
   public static void main(final String[] args) {
-	App app = new App();
+	final App app = new App();
 
-	new ActvStack(app, "CdkTemplateStack", StackProps.builder()
-		// If you don't specify 'env', this stack will be environment-agnostic.
-		// Account/Region-dependent features and context lookups will not work,
-		// but a single synthesized template can be deployed anywhere.
+	var awsAccountId = Optional.ofNullable(System.getenv("AWS_ACCOUNT_ID"));
+	var awsRegion = Optional.ofNullable(System.getenv("AWS_REGION"));
 
-		// Uncomment the next block to specialize this stack for the AWS Account
-		// and Region that are implied by the current CLI configuration.
-
-//		.env(Environment.builder()
-//				.account(System.getenv("CDK_DEFAULT_ACCOUNT"))
-//				.region(System.getenv("CDK_DEFAULT_REGION"))
-//				.build())
-
-
-		// Uncomment the next block if you know exactly what Account and Region you
-		// want to deploy the stack to.
-
-		.env(Environment.builder()
-				.account("039612889815")
-				.region("us-east-1")
-				.build())
-
-
-		// For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-		.build());
+	final ActvStack actvStack = new ActvStack(app, "ActvStack",
+		StackProps.builder()
+			.env(Environment.builder()
+				.account(awsAccountId.orElse(System.getenv("CDK_DEFAULT_ACCOUNT")))
+				.region(awsRegion.orElse(System.getenv("CDK_DEFAULT_REGION")))
+				.build()
+			)
+			.description("Stack defining AWS infrastructure for Ayden Chance Television (ACTV)")
+			.build()
+	);
 
 	app.synth();
   }
