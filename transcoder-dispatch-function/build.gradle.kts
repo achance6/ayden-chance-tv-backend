f798c9c247d7c861420212b1de29fbc0f20d74a7
achance6 <ayden.chance@rocketmail.com>
2025-08-13 23:29:42 -0400

    resolve warnings

--- transcoder-dispatch-function/build.gradle.kts
+++ transcoder-dispatch-function/build.gradle.kts
@@ -3,17 +3,23 @@ plugins {
 }
 
 val quarkusPlatformVersion: String by project
+val mockitoAgent: Configuration by configurations.creating
 
 dependencies {
     implementation(enforcedPlatform("io.quarkus.platform:quarkus-bom:$quarkusPlatformVersion"))
     implementation(enforcedPlatform("io.quarkus.platform:quarkus-amazon-services-bom:$quarkusPlatformVersion"))
     implementation("io.quarkus:quarkus-amazon-lambda")
 
+    // Excluding unneeded clients. Using aws-crt-client.
     implementation("software.amazon.awssdk:mediaconvert") {
         exclude(group = "software.amazon.awssdk", module = "apache-client")
         exclude(group = "software.amazon.awssdk", module = "netty-nio-client")
     }
     implementation("software.amazon.awssdk:aws-crt-client")
+
+    // Adapter for routing Apache Commons logging to JBoss Log Manager
+    // Needed due to AWS SDKs using Apache Commons
+    implementation("org.jboss.logging:commons-logging-jboss-logging")
 
     testImplementation("io.quarkus:quarkus-junit5-mockito")
     testImplementation("io.rest-assured:rest-assured")
@@ -22,9 +28,17 @@ dependencies {
     testImplementation("com.amazonaws:aws-lambda-java-tests:1.1.1")
     testImplementation("com.amazonaws:aws-lambda-java-serialization:1.1.6")
 
-    // Adapter for routing Apache Commons logging to JBoss Log Manager
-    // Needed due to AWS SDKs using Apache Commons
-    implementation("org.jboss.logging:commons-logging-jboss-logging")
+    // Resolves warning when running tests on Apple Silicon
+    testRuntimeOnly("io.netty:netty-resolver-dns-native-macos:4.2.3.Final:osx-aarch_64")
+
+    // Resolves JVM warning concerning dynamically attaching agents
+    mockitoAgent("org.mockito:mockito-core:5.18.0") {
+        isTransitive = false
+    }
+}
+
+tasks.withType<Test> {
+    jvmArgs("-javaagent:${mockitoAgent.asPath}")
 }
 
 group = "com.chance.ayden.transcoderdispatchfunction"
